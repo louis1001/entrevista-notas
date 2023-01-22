b9f322d831087ac575d8ac69175d2899bbe9fe5b
louis1001 <louis1001dev@gmail.com>
2023-01-21 23:15:56 -0600

    Sets up a PersistanceController to manage code data behavior

---
+++ simple-notes/Source/DataManagement/PersistenceController.swift
@@ -0,0 +1,42 @@
+//
+//  PersistenceController.swift
+//  simple-notes
+//
+//  Created by Luis Gonzalez on 21/1/23.
+//
+
+import CoreData
+import Foundation
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+    
+    let container: NSPersistentContainer
+    
+    private init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "Main")
+        
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        
+        container.loadPersistentStores { description, error in
+            if let error {
+                fatalError("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func save() {
+        let context = container.viewContext
+        
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Handle error later
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
