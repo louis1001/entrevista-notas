f9c2a837e60610d47582a1472eb6b99e29f003dc
louis1001 <louis1001dev@gmail.com>
2023-01-24 19:32:28 -0600

    Adds basic styles for the list rows - setup constants

---
+++ simple-notes/Source/Helpers/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  simple-notes
+//
+//  Created by Luis Gonzalez on 24/1/23.
+//
+
+import Foundation
+
+#if os(macOS)
+let IS_MAC = true
+#else
+let IS_MAC = false
+#endif
--- simple-notes/Source/Views/Components/NotaListItem.swift
+++ simple-notes/Source/Views/Components/NotaListItem.swift
@@ -7,8 +7,12 @@
 
 import SwiftUI
 
+let CONTENT_FONT_NAME = "Noto Sans Armenian Regular"
+
 struct NotaListItem: View {
     var nota: Nota
+    
+    private static let contentFontSize: CGFloat = IS_MAC ? 10 : 14
     
     private var titulo: String {
         nota.titulo.isEmpty
@@ -18,25 +22,26 @@ struct NotaListItem: View {
     
     private var contenido: String {
         nota.contenido.isEmpty
-        ? "Sin contenido"
+        ? "sin contenido"
         : nota.contenido
     }
     
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 5) {
             Text(titulo)
-                .font(.title2)
+                .font(.body)
                 .bold()
             
-            // + "\n\n" Para que el Text siempre tenga la algura máxima de 2 lineas
-            Text(contenido + "\n\n")
-                .font(.system(size: 10))
+            Text(contenido + "\n\n") // Saltos de linea para que siempre tenga el tamaño máximo
                 .lineLimit(2)
-                .opacity(0.7)
+                .font(.custom(CONTENT_FONT_NAME, size: Self.contentFontSize))
+                .opacity(0.8)
+                .fixedSize(horizontal: false, vertical: true)
             
             Text(nota.ultimaEdicion.formatted())
+                .font(.system(size: 10))
                 .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
-                .font(.footnote)
         }
+        .frame(height: IS_MAC ? 70 : 80, alignment: .topLeading)
     }
 }
