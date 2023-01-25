c6d41205aed932112c2e24441f6f67efb878ab7f
louis1001 <louis1001dev@gmail.com>
2023-01-24 19:51:07 -0600

    Add custom text view (from a previous project)

--- simple-notes/Source/Helpers/Extensions/View+Extensions.swift
+++ simple-notes/Source/Helpers/Extensions/View+Extensions.swift
@@ -12,4 +12,16 @@ extension View {
         /// Helper para convertir vista en un botón.
         Button(action: action, label: {self})
     }
+    
+    /// Aplica modificadores condicionalmente
+    @ViewBuilder
+    func `if`(_ condition: Bool, then: (Self) -> some View, else otherwise: ((Self) -> some View)? = nil) -> some View {
+        if condition {
+            then(self)
+        } else if let otherwise {
+            otherwise(self)
+        } else {
+            self
+        }
+    }
 }
