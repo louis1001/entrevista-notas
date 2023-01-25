a6fd157f2e32a7a8477db49ccc8e432cef1b06aa
louis1001 <louis1001dev@gmail.com>
2023-01-24 18:56:16 -0600

    Sets up basic Navigation List and Detail styles

--- simple-notes/Source/Views/MainView.swift
+++ simple-notes/Source/Views/MainView.swift
@@ -13,13 +13,30 @@ struct MainView: View {
     
     @State private var pickingSort = false
     
+    @State private var columnVisibility = NavigationSplitViewVisibility.automatic
+    
     var body: some View {
-        NavigationSplitView(sidebar: {sidebar}, detail: {detail})
+        NavigationSplitView(columnVisibility: $columnVisibility, sidebar: {sidebar}, detail: {detail})
         .searchable(text: $viewModel.searchQuery)
+        #if os(macOS)
+        .frame(minWidth: 700, minHeight: 200)
+        .navigationSplitViewStyle(.prominentDetail)
+        .onAppear {
+            columnVisibility = .all
+        }
+        #else
+        .navigationSplitViewStyle(.automatic)
+        #endif
     }
     
     var sidebar: some View {
         NotaList(viewModel: viewModel, selection: $selection)
+#if os(macOS)
+    .navigationSplitViewColumnWidth(min: 100, ideal: 200, max: 350)
+#else
+    .navigationSplitViewColumnWidth(min: 100, ideal: 400, max: 400)
+#endif
+    .navigationTitle("Mis Notas")
     }
     
     @ViewBuilder
@@ -29,6 +46,11 @@ struct MainView: View {
                 Task { await viewModel.updateNota(nota) }
             }
                 .id(selection.id)
+        } else {
+            VStack {
+                Text("Abre una nota")
+            }
+                .id(-123)
         }
     }
 }
